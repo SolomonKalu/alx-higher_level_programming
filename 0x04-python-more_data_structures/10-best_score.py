3991d2da84600ee57ac2864e1f44f18c4d87c1c3
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2024-01-12 14:03:43 -0800

    Fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return None
-    large = max(a_dictionary.values())
-    for x, value in a_dictionary.items():
-	if value is large:
-	    return x
+    if not a_dictionary:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))
